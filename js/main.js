bc9c9354e83a003a49952fb1d7d6056d3281c4a9
Mario Morelos <m.d.29@hotmail.com>
2026-02-28 19:50:07 -0600

    invitacion v1

---
+++ js/main.js
@@ -0,0 +1,79 @@
+const params = new URLSearchParams(window.location.search);
+const name = atob(params.get("n"));
+const age = atob(params.get("i"));
+
+// console.log(`Name: ${name}, Age: ${age}`);
+
+document.getElementById("name").innerHTML = name;
+document.getElementById("num_invitados").innerHTML = age + " invitados";
+
+const eventDate = new Date("May 1, 2026 17:00:00").getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const distance = eventDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+  const hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+  const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+  const seconds = Math.floor(distance % (1000 * 60) / 1000);
+
+  // document.getElementById("timer").innerHTML =
+  //   days + " días, " + hours + " horas, " + minutes + " minutos y " + seconds + " segundos restantes";
+  document.getElementById("dias").innerHTML = days;
+  document.getElementById("horas").innerHTML = hours;
+  document.getElementById("minutos").innerHTML = minutes;
+  document.getElementById("segundos").innerHTML = seconds;
+}, 1000);
+
+flatpickr("#fecha", {
+  defaultDate: "2026-05-01",
+  enable: ["2026-05-01"]
+});
+
+document.getElementById("mostrar-card").addEventListener("click", () => {
+  const card = document.getElementById("datos-card");
+  card.classList.remove("d-none");
+  document.getElementById("mostrar-card").classList.add("d-none");
+});
+
+
+const btn = document.getElementById("music-btn");
+const audio = document.getElementById("bg-music");
+const icon = document.getElementById("icon");
+
+btn.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.play();
+    icon.textContent = "❚❚";
+    btn.classList.add("playing");
+  } else {
+    audio.pause();
+    icon.textContent = "▶";
+    btn.classList.remove("playing");
+  }
+});
+
+const envelope = document.getElementById("envelope");
+const intro = document.getElementById("intro");
+const contenido = document.getElementById("contenido");
+
+envelope.addEventListener("click", () => {
+  envelope.classList.add("open");
+
+  setTimeout(() => {
+    intro.classList.add("d-none");
+    document.body.style.overflow = "auto";
+    contenido.classList.add("visible");
+    audio.play();
+  }, 1000);
+});
+
+
+function enviarWhatsApp() {
+  const nombre = "Mario";
+  const mensaje = `Hola, soy ${nombre}. Confirmo mi asistencia a la boda.`;
+  const url = `https://wa.me/525516503708?text=${encodeURIComponent(mensaje)}`;
+  window.open(url, "_blank");
+}
